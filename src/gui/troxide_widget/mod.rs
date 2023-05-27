4cb8d94eba00d3f98023d6d64a35c4f1ded8d049
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-05-28 02:09:36 +0300

    refactor code

--- src/gui/troxide_widget/mod.rs
+++ src/gui/troxide_widget/mod.rs
@@ -6,6 +6,8 @@ use iced::{
     Alignment, Renderer,
 };
 
+pub mod series_page;
+
 // The text size of the beginning part of a info
 const INFO_HEADER: u16 = 18;
 // The text size of the main part of a info
@@ -13,7 +15,6 @@ const INFO_BODY: u16 = 15;
 
 // const INFO_BODY_HEIGHT: u16 = INFO_HEADER - (INFO_HEADER - INFO_BODY);
 
-pub mod series_page;
 /// Generates the SeriesSearchResult widget
 pub fn series_result(
     series_result: &series_searching::SeriesSearchResult,
