ddfe22837c6f95f5a36f24f8490b3bea1034459c
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-06-20 22:50:38 +0300

    add config file loading and saving functionality

--- src/core/settings_config.rs
+++ src/core/settings_config.rs
@@ -1,3 +1,6 @@
+use std::io::ErrorKind;
+
+use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize, Eq, PartialEq)]
@@ -25,8 +28,52 @@ pub struct Config {
     pub theme: Theme,
 }
 
+const CONFIG_FILE_NAME: &str = "config.toml";
+
 pub fn load_config() -> Config {
-    Config::default()
+    if let Some(proj_dirs) = ProjectDirs::from("", "", env!("CARGO_PKG_NAME")) {
+        let mut config_file = std::path::PathBuf::from(proj_dirs.config_dir());
+        config_file.push(CONFIG_FILE_NAME);
+
+        let file_contents = match std::fs::read_to_string(&config_file) {
+            Ok(file_contents) => file_contents,
+            Err(err) => {
+                let default_config = Config::default();
+                if let ErrorKind::NotFound = err.kind() {
+                    std::fs::write(
+                        config_file,
+                        toml::to_string_pretty(&default_config).unwrap(),
+                    )
+                    .expect(&format!("Could not write default config file: {}", err));
+                }
+                return default_config;
+            }
+        };
+
+        match toml::from_str(&file_contents) {
+            Ok(config) => config,
+            Err(err) => {
+                eprintln!("could not parse the config file: {}", err);
+                return Config::default();
+            }
+        }
+    } else {
+        eprintln!("could not obtain config directory path");
+        Config::default()
+    }
 }
 
-// pub fn save_config(settings_config: &Config) {}
+pub fn save_config(settings_config: &Config) {
+    if let Some(proj_dirs) = ProjectDirs::from("", "", env!("CARGO_PKG_NAME")) {
+        let mut config_file = std::path::PathBuf::from(proj_dirs.config_dir());
+        config_file.push(CONFIG_FILE_NAME);
+
+        std::fs::write(
+            config_file,
+            toml::to_string_pretty(&settings_config).unwrap(),
+        )
+        .expect(&format!("Could not write default config file"));
+    } else {
+        eprintln!("could not obtain config directory path");
+    }
+}
