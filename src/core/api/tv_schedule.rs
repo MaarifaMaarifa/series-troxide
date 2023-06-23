2208212980c9e6419674425a26e40c38e964b65a
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-06-24 01:03:27 +0300

    remove duplicates when retrieving scheduled episodes.
    since some shows can release multiple episodes at once, their episodes
    can appear duplicated in the discover view as it is concerned with
    displaying just the show posters and not individual episodes. This change
    fixes that to some extent, show posters will still be duplicated if
    they are not consecutive to each other as this depends on the
    dedup_by_key method of a vector.

--- src/core/api/tv_schedule.rs
+++ src/core/api/tv_schedule.rs
@@ -17,5 +17,8 @@ pub async fn get_episodes_with_date(date: Option<&str>) -> Result<Vec<Episode>, 
         .await
         .map_err(ApiError::Network)?;
 
-    deserialize_json(&prettified_json)
+    deserialize_json::<Vec<Episode>>(&prettified_json).map(|mut episodes| {
+        episodes.dedup_by_key(|episode| episode.links.show.href.clone());
+        episodes
+    })
 }
