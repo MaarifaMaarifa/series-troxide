d308ab11e9ed0a61d9d36d588240a92c65f4da5d
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-07-09 21:18:12 +0300

    add white theme

--- src/gui/styles/container_styles.rs
+++ src/gui/styles/container_styles.rs
@@ -1,3 +1,4 @@
+use super::theme::TroxideTheme;
 use iced::widget::container::{Appearance, StyleSheet};
 use iced::Background;
 use iced::{color, Color};
@@ -7,13 +8,26 @@ pub struct ContainerThemeFirst;
 impl StyleSheet for ContainerThemeFirst {
     type Style = iced::Theme;
 
-    fn appearance(&self, _style: &Self::Style) -> Appearance {
-        Appearance {
-            background: Some(Background::Color(color!(0x1c1c1c))),
-            border_color: Color::BLACK,
+    fn appearance(&self, style: &Self::Style) -> Appearance {
+        let mut appearance = Appearance {
             border_width: 1.0,
             border_radius: 10.0,
             ..Appearance::default()
+        };
+
+        match style {
+            iced::Theme::Custom(custom) => {
+                if **custom == TroxideTheme::get_theme(&TroxideTheme::Light) {
+                    appearance.background = Some(Background::Color(color!(0xcccccc)));
+                    appearance.border_color = color!(0xbbbbbb);
+                    appearance
+                } else {
+                    appearance.background = Some(Background::Color(color!(0x1c1c1c)));
+                    appearance.border_color = Color::BLACK;
+                    appearance
+                }
+            }
+            _ => unreachable!("built-in iced themes are not in use"),
         }
     }
 }
@@ -23,13 +37,26 @@ pub struct ContainerThemeSecond;
 impl StyleSheet for ContainerThemeSecond {
     type Style = iced::Theme;
 
-    fn appearance(&self, _style: &Self::Style) -> Appearance {
-        Appearance {
-            background: Some(Background::Color(color!(0x282828))),
-            border_color: Color::BLACK,
+    fn appearance(&self, style: &Self::Style) -> Appearance {
+        let mut appearance = Appearance {
             border_width: 1.0,
             border_radius: 10.0,
             ..Appearance::default()
+        };
+
+        match style {
+            iced::Theme::Custom(custom) => {
+                if **custom == TroxideTheme::get_theme(&TroxideTheme::Light) {
+                    appearance.background = Some(Background::Color(color!(0xbbbbbb)));
+                    appearance.border_color = color!(0xbbbbbb);
+                    appearance
+                } else {
+                    appearance.background = Some(Background::Color(color!(0x282828)));
+                    appearance.border_color = Color::BLACK;
+                    appearance
+                }
+            }
+            _ => unreachable!("built-in iced themes are not in use"),
         }
     }
 }
@@ -39,13 +66,28 @@ pub struct ContainerThemeReleaseTime;
 impl StyleSheet for ContainerThemeReleaseTime {
     type Style = iced::Theme;
 
-    fn appearance(&self, _style: &Self::Style) -> Appearance {
-        Appearance {
-            background: Some(Background::Color(Color::from_rgb(0.5, 0.5, 0.0))),
-            border_color: Color::BLACK,
+    fn appearance(&self, style: &Self::Style) -> Appearance {
+        let mut appearance = Appearance {
+            background: Some(Background::Color(color!(0x8f6593))),
+            border_color: color!(0xbbbbbb),
             border_width: 1.0,
             border_radius: 1000.0, // Making sure it is circular
             ..Appearance::default()
+        };
+
+        match style {
+            iced::Theme::Custom(custom) => {
+                if **custom == TroxideTheme::get_theme(&TroxideTheme::Light) {
+                    appearance.background = Some(Background::Color(color!(0x8f6593)));
+                    appearance.border_color = color!(0xbbbbbb);
+                    appearance
+                } else {
+                    appearance.background = Some(Background::Color(color!(0x8f6593)));
+                    appearance.border_color = Color::BLACK;
+                    appearance
+                }
+            }
+            _ => unreachable!("built-in iced themes are not in use"),
         }
     }
 }
--- src/gui/styles/theme.rs
+++ src/gui/styles/theme.rs
@@ -12,16 +12,16 @@ impl TroxideTheme {
     pub fn get_theme(&self) -> Custom {
         match self {
             TroxideTheme::Light => Custom::new(Palette {
-                background: Color::from_rgb(1.0, 0.9, 1.0),
+                background: color!(0xdddddd),
                 text: Color::BLACK,
-                primary: Color::from_rgb(0.5, 0.5, 0.0),
+                primary: color!(0x8f6593),
                 success: Color::from_rgb(0.0, 1.0, 0.0),
                 danger: Color::from_rgb(1.0, 0.0, 0.0),
             }),
             TroxideTheme::Dark => Custom::new(Palette {
                 background: color!(0x161616),
                 text: color!(0xcccccc),
-                primary: Color::from_rgb(0.5, 0.5, 0.0),
+                primary: color!(0x8f6593),
                 success: Color::from_rgb(0.0, 1.0, 0.0),
                 danger: Color::from_rgb(1.0, 0.0, 0.0),
             }),
