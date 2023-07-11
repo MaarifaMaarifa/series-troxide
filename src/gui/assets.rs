85afc0c822e1b883e01e233decbd9a766fb446dc
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-07-11 21:44:42 +0300

    move icons to the root asset folder

--- src/gui/assets.rs
+++ src/gui/assets.rs
@@ -8,22 +8,15 @@ pub fn get_static_cow_from_asset(static_asset: &'static [u8]) -> Cow<'static, [u
 }
 
 pub mod icons {
-    pub static ARROW_BAR_UP: &[u8; 376] = include_bytes!("assets/icons/arrow-bar-up.svg");
-    pub static ARROW_BAR_DOWN: &[u8; 375] = include_bytes!("assets/icons/arrow-bar-down.svg");
-    pub static ARROW_LEFT: &[u8; 311] = include_bytes!("assets/icons/arrow-left.svg");
-    pub static BINOCULARS_FILL: &[u8; 639] = include_bytes!("assets/icons/binoculars-fill.svg");
-    pub static CARD_CHECKLIST: &[u8; 730] = include_bytes!("assets/icons/card-checklist.svg");
-    pub static FILM: &[u8; 384] = include_bytes!("assets/icons/film.svg");
-    pub static GRAPH_UP_ARROW: &[u8; 402] = include_bytes!("assets/icons/graph-up-arrow.svg");
-    pub static SEARCH: &[u8; 331] = include_bytes!("assets/icons/search.svg");
-    pub static CHECK_CIRCLE: &[u8; 387] = include_bytes!("assets/icons/check-circle.svg");
-    pub static CHECK_CIRCLE_FILL: &[u8; 340] = include_bytes!("assets/icons/check-circle-fill.svg");
-    pub static GEAR_WIDE_CONNECTED: &[u8; 1312] =
-        include_bytes!("assets/icons/gear-wide-connected.svg");
+    pub static ARROW_BAR_UP: &[u8; 376] = include_bytes!("../../assets/icons/arrow-bar-up.svg");
+    pub static ARROW_BAR_DOWN: &[u8; 375] = include_bytes!("../../assets/icons/arrow-bar-down.svg");
+    pub static ARROW_LEFT: &[u8; 311] = include_bytes!("../../assets/icons/arrow-left.svg");
+    pub static CHECK_CIRCLE: &[u8; 387] = include_bytes!("../../assets/icons/check-circle.svg");
+    pub static CHECK_CIRCLE_FILL: &[u8; 340] =
+        include_bytes!("../../assets/icons/check-circle-fill.svg");
 }
 
 pub mod fonts {
-
     pub static NOTOSANS_REGULAR_STATIC: &[u8; 556216] =
         include_bytes!("../../assets/fonts/NotoSans-Regular.ttf");
 }
