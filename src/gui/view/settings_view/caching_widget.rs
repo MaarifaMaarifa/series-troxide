ebdd13870d359bc29ef550b5baf3c90ed8351082
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-07-20 01:49:06 +0300

    refactor settings page

---
+++ src/gui/view/settings_view/caching_widget.rs
@@ -0,0 +1,191 @@
+use iced::widget::{button, column, container, horizontal_space, row, text, Button, Text};
+use iced::{Command, Element, Length, Renderer};
+
+use crate::core::caching::cache_cleaning;
+use crate::gui::styles;
+
+#[allow(clippy::enum_variant_names)] // Removing the word clean makes the message not make sense
+#[derive(Clone, Debug)]
+pub enum Message {
+    CleanEndedCache,
+    CleanWaitingReleaseCache,
+    CleanAiredCache,
+    CleanEndedCacheComplete(Option<String>),
+    CleanWaitingReleaseCacheComplete(Option<String>),
+    CleanAiredCacheComplete(Option<String>),
+    // timeouts for removing the status text
+    CleanEndedCacheTimeoutComplete,
+    CleanWaitingReleaseCacheTimeoutComplete,
+    CleanAiredCacheTimeoutComplete,
+}
+
+#[derive(Default)]
+enum CleaningStatus {
+    #[default]
+    Idle,
+    Running,
+    Done(Option<String>),
+}
+
+#[derive(Default)]
+pub struct Caching {
+    ended_series_cache_cleaning: CleaningStatus,
+    aired_series_cache_cleaning: CleaningStatus,
+    waiting_release_series_cache_cleaning: CleaningStatus,
+}
+
+impl Caching {
+    pub fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::CleanEndedCache => {
+                self.ended_series_cache_cleaning = CleaningStatus::Running;
+                return Command::perform(cache_cleaning::clean_ended_series_cache(), |res| {
+                    Message::CleanEndedCacheComplete(res.err().map(|err| err.to_string()))
+                });
+            }
+            Message::CleanWaitingReleaseCache => {
+                self.waiting_release_series_cache_cleaning = CleaningStatus::Running;
+                return Command::perform(
+                    cache_cleaning::clean_running_cache(
+                        cache_cleaning::RunningStatus::WaitingRelease,
+                    ),
+                    |res| {
+                        Message::CleanWaitingReleaseCacheComplete(
+                            res.err().map(|err| err.to_string()),
+                        )
+                    },
+                );
+            }
+            Message::CleanAiredCache => {
+                self.aired_series_cache_cleaning = CleaningStatus::Running;
+                return Command::perform(
+                    cache_cleaning::clean_running_cache(cache_cleaning::RunningStatus::Aired),
+                    |res| Message::CleanAiredCacheComplete(res.err().map(|err| err.to_string())),
+                );
+            }
+            Message::CleanEndedCacheComplete(error_text) => {
+                self.ended_series_cache_cleaning = CleaningStatus::Done(error_text);
+                return Command::perform(super::status_timeout(), |_| {
+                    Message::CleanEndedCacheTimeoutComplete
+                });
+            }
+            Message::CleanWaitingReleaseCacheComplete(error_text) => {
+                self.waiting_release_series_cache_cleaning = CleaningStatus::Done(error_text);
+                return Command::perform(super::status_timeout(), |_| {
+                    Message::CleanWaitingReleaseCacheTimeoutComplete
+                });
+            }
+            Message::CleanAiredCacheComplete(error_text) => {
+                self.aired_series_cache_cleaning = CleaningStatus::Done(error_text);
+                return Command::perform(super::status_timeout(), |_| {
+                    Message::CleanAiredCacheTimeoutComplete
+                });
+            }
+            Message::CleanEndedCacheTimeoutComplete => {
+                self.ended_series_cache_cleaning = CleaningStatus::Idle
+            }
+            Message::CleanWaitingReleaseCacheTimeoutComplete => {
+                self.waiting_release_series_cache_cleaning = CleaningStatus::Idle
+            }
+            Message::CleanAiredCacheTimeoutComplete => {
+                self.aired_series_cache_cleaning = CleaningStatus::Idle
+            }
+        }
+        Command::none()
+    }
+
+    pub fn view(&self) -> Element<'_, Message, Renderer> {
+        let content = column![
+            text("Series Troxide Cache")
+                .size(25)
+                .style(styles::text_styles::purple_text_theme()),
+            self.clean_aired_cache_widget(),
+            self.clean_waiting_release_cache_widget(),
+            self.clean_ended_cache_widget(),
+        ]
+        .padding(5)
+        .spacing(5);
+
+        container(content)
+            .style(styles::container_styles::first_class_container_theme())
+            .width(1000)
+            .into()
+    }
+
+    fn clean_ended_cache_widget(&self) -> Element<'_, Message, Renderer> {
+        let (status_text, button) =
+            status_and_button_gen(&self.ended_series_cache_cleaning, Message::CleanEndedCache);
+        column![
+            text("Ended Cache Cleaning").size(22),
+            row![
+                text("clean cache for the series that have ended").size(15),
+                horizontal_space(Length::Fill),
+                status_text,
+                button,
+            ]
+            .spacing(5)
+        ]
+        .into()
+    }
+
+    fn clean_aired_cache_widget(&self) -> Element<'_, Message, Renderer> {
+        let (status_text, button) =
+            status_and_button_gen(&self.aired_series_cache_cleaning, Message::CleanAiredCache);
+
+        column![
+            text("Aired Cache Cleaning").size(22),
+            row![
+                text("clean cache for the series that are currently being aired").size(15),
+                horizontal_space(Length::Fill),
+                status_text,
+                button,
+            ]
+            .spacing(5)
+        ]
+        .into()
+    }
+
+    fn clean_waiting_release_cache_widget(&self) -> Element<'_, Message, Renderer> {
+        let (status_text, button) = status_and_button_gen(
+            &self.waiting_release_series_cache_cleaning,
+            Message::CleanWaitingReleaseCache,
+        );
+
+        column![
+            text("Waiting Release Cache Cleaning").size(22),
+            row![
+                text("clean cache for the series waiting for their release date").size(15),
+                horizontal_space(Length::Fill),
+                status_text,
+                button,
+            ]
+            .spacing(5)
+        ]
+        .into()
+    }
+}
+
+/// Generates the status text and the button for each type of cleaning
+fn status_and_button_gen(
+    cleaning_status: &CleaningStatus,
+    button_message: Message,
+) -> (Text, Button<Message, Renderer>) {
+    let mut button = button("clean");
+    let status_text = match cleaning_status {
+        CleaningStatus::Idle => {
+            button = button.on_press(button_message);
+            text("")
+        }
+        CleaningStatus::Running => text("Running"),
+        CleaningStatus::Done(error_message) => {
+            button = button.on_press(button_message);
+            if let Some(error_message) = error_message {
+                text(error_message.as_str()).style(styles::text_styles::red_text_theme())
+            } else {
+                text("Done!").style(styles::text_styles::green_text_theme())
+            }
+        }
+    };
+
+    (status_text, button)
+}
---
+++ src/gui/view/settings_view/database_widget.rs
@@ -0,0 +1,132 @@
+use iced::widget::{button, column, container, horizontal_space, row, text, Text};
+use iced::{Command, Element, Length, Renderer};
+
+use crate::gui::styles;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    ImportDatabasePressed,
+    ExportDatabasePressed,
+    ImportTimeoutComplete,
+    ExportTimeoutComplete,
+}
+
+#[derive(Default)]
+pub struct Database {
+    import_status: Option<anyhow::Result<()>>,
+    export_status: Option<anyhow::Result<()>>,
+}
+
+impl Database {
+    pub fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::ImportDatabasePressed => {
+                self.import_status = Some(database_transfer::import());
+
+                Command::perform(super::status_timeout(), |_| Message::ImportTimeoutComplete)
+            }
+            Message::ExportDatabasePressed => {
+                self.export_status = Some(database_transfer::export());
+                Command::perform(super::status_timeout(), |_| Message::ExportTimeoutComplete)
+            }
+            Message::ImportTimeoutComplete => {
+                self.import_status = None;
+                Command::none()
+            }
+            Message::ExportTimeoutComplete => {
+                self.export_status = None;
+                Command::none()
+            }
+        }
+    }
+
+    pub fn view(&self) -> Element<'_, Message, Renderer> {
+        let import_widget = column![
+            text("Import Data").size(22),
+            row![
+                text("Import your series tracking data into Series Troxide").size(15),
+                horizontal_space(Length::Fill),
+                get_status_text(self.import_status.as_ref()),
+                button("Import").on_press(Message::ImportDatabasePressed)
+            ]
+            .spacing(5)
+        ];
+
+        let export_widget = column![
+            text("Export Data").size(22),
+            row![
+                text("Export your series tracking data from Series Troxide").size(15),
+                horizontal_space(Length::Fill),
+                get_status_text(self.export_status.as_ref()),
+                button("Export").on_press(Message::ExportDatabasePressed)
+            ]
+            .spacing(5)
+        ];
+
+        let content = column![
+            text("Series Troxide Data")
+                .size(25)
+                .style(styles::text_styles::purple_text_theme()),
+            import_widget,
+            export_widget,
+        ]
+        .padding(5)
+        .spacing(5);
+
+        container(content)
+            .style(styles::container_styles::first_class_container_theme())
+            .width(1000)
+            .into()
+    }
+}
+
+fn get_status_text(status: Option<&anyhow::Result<()>>) -> Text {
+    if let Some(res) = status {
+        if let Err(err) = res {
+            text(err.to_string()).style(styles::text_styles::red_text_theme())
+        } else {
+            text("Done!").style(styles::text_styles::green_text_theme())
+        }
+    } else {
+        text("")
+    }
+}
+
+mod database_transfer {
+    use directories::UserDirs;
+    use std::path;
+
+    use crate::core::database::database_transfer;
+    use rfd::FileDialog;
+
+    pub fn export() -> anyhow::Result<()> {
+        let chosen_path = FileDialog::new()
+            .set_directory(get_home_directory()?)
+            .save_file();
+
+        if let Some(mut chosen_path) = chosen_path {
+            let file_name = chosen_path.file_name().map(std::ffi::OsString::from);
+            chosen_path.pop();
+            database_transfer::write_database_to_path(&chosen_path, file_name.as_deref())?;
+        }
+
+        Ok(())
+    }
+
+    pub fn import() -> anyhow::Result<()> {
+        let chosen_path = FileDialog::new()
+            .set_directory(get_home_directory()?)
+            .pick_file();
+
+        if let Some(chosen_path) = chosen_path {
+            database_transfer::read_database_from_path(path::Path::new(&chosen_path))?;
+        }
+
+        Ok(())
+    }
+
+    pub fn get_home_directory() -> anyhow::Result<path::PathBuf> {
+        let user_dirs = UserDirs::new().ok_or(anyhow::anyhow!("could not get user directory"))?;
+        Ok(user_dirs.home_dir().to_path_buf())
+    }
+}
