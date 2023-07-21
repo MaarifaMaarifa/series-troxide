07e055ed33703be599a1a21bba6738d9937c99a7
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-07-21 18:04:27 +0300

    made panic happen with useful message

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> anyhow::Result<()> {
 
     let cache_settings = core::settings_config::SETTINGS
         .read()
-        .unwrap()
+        .expect("could not read the program settigns")
         .get_current_settings()
         .cache
         .clone();
