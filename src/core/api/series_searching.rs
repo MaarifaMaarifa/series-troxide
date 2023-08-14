5b78a82ea5cd3933c84cb4ccc4ee310feb6aff8b
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-08-15 02:10:13 +0300

    improve series searching
    realized that the search result included `series information` which
    was already defined earlier, and therefore just reused an already
    defined structure.

--- src/core/api/series_searching.rs
+++ src/core/api/series_searching.rs
@@ -8,16 +8,7 @@ const SERIES_SEARCH_ADDRESS: &str = "https://api.tvmaze.com/search/shows?q=";
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct SeriesSearchResult {
-    pub show: Show,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct Show {
-    pub id: u32,
-    pub name: String,
-    pub premiered: Option<String>,
-    pub genres: Vec<String>,
-    pub image: Option<Image>,
+    pub show: series_information::SeriesMainInformation,
 }
 
 pub async fn search_series(series_name: String) -> Result<Vec<SeriesSearchResult>, ApiError> {
