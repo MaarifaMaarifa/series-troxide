30bcf2327d0e55eeee43b261fac00d4e10c38abf
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-10-01 00:41:54 +0300

    cargo fmt

--- src/gui/assets.rs
+++ src/gui/assets.rs
@@ -17,8 +17,7 @@ pub mod icons {
     pub static STAR_HALF: &[u8] = include_bytes!("../../assets/icons/star-half.svg");
     pub static PATCH_PLUS: &[u8] = include_bytes!("../../assets/icons/patch-plus.svg");
     pub static PATCH_PLUS_FILL: &[u8] = include_bytes!("../../assets/icons/patch-plus-fill.svg");
-    pub static TRAKT_ICON_RED: &[u8] =
-        include_bytes!("../../assets/logos/trakt-icon-red.svg");
+    pub static TRAKT_ICON_RED: &[u8] = include_bytes!("../../assets/logos/trakt-icon-red.svg");
 }
 
 pub mod fonts {
