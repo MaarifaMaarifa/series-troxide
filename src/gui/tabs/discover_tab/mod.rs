1b18f85b52308a0137069100caa7bd2dc9667ab4
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2023-12-30 00:42:39 +0300

    Center discover page spinner (#127)
    * Center discover page spinner
    
    * Code refactor
    
    * Remove unused import

--- src/gui/tabs/discover_tab/mod.rs
+++ src/gui/tabs/discover_tab/mod.rs
@@ -7,10 +7,10 @@ use full_schedule::{FullSchedulePosters, Message as FullSchedulePostersMessage};
 use searching::Message as SearchMessage;
 
 use iced::widget::scrollable::{RelativeOffset, Viewport};
-use iced::widget::{column, scrollable, Space};
+use iced::widget::{column, container, scrollable, Space};
 use iced::{Command, Element, Length, Renderer};
 
-use iced_aw::floating_element;
+use iced_aw::{floating_element, Spinner};
 
 use super::Tab;
 
@@ -91,26 +91,32 @@ impl<'a> DiscoverTab<'a> {
     }
 
     pub fn view(&self) -> Element<'_, Message, Renderer> {
-        let underlay: Element<'_, Message, Renderer> = scrollable(
-            self.full_schedule_series
-                .view()
-                .map(Message::FullSchedulePosters),
-        )
-        .direction(styles::scrollable_styles::vertical_direction())
-        .id(Self::scrollable_id())
-        .on_scroll(Message::PageScrolled)
-        .width(Length::Fill)
-        .into();
+        let underlay: Element<'_, Message, Renderer> =
+            if let Some(full_schedule_series) = self.full_schedule_series.view() {
+                scrollable(full_schedule_series.map(Message::FullSchedulePosters))
+                    .direction(styles::scrollable_styles::vertical_direction())
+                    .id(Self::scrollable_id())
+                    .on_scroll(Message::PageScrolled)
+                    .width(Length::Fill)
+                    .into()
+            } else {
+                container(Spinner::new())
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_x()
+                    .center_y()
+                    .into()
+            };
 
-        let content = floating_element::FloatingElement::new(
-            underlay,
-            self.search
-                .view()
-                .1
-                .map(|element| element.map(Message::Search))
-                .unwrap_or(Space::new(0, 0).into()),
-        )
-        .anchor(floating_element::Anchor::North);
+        let overlay = self
+            .search
+            .view()
+            .1
+            .map(|element| element.map(Message::Search))
+            .unwrap_or(Space::new(0, 0).into());
+
+        let content = floating_element::FloatingElement::new(underlay, overlay)
+            .anchor(floating_element::Anchor::North);
 
         column![self.search.view().0.map(Message::Search), content]
             .spacing(2)
